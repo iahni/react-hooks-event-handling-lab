8407e791eb454aafe7393323b83404c5e92f3ad1
james <astonjames@iahni.com>
2023-06-04 23:00:22 -0600

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
-// Code EyesOnMe Component Here
+import react from "react";
+
+function EyesOnMe(){
+    return(
+        <div>
+            <button onFocus={()=>{console.log("Good!")}} onBlur={()=>{console.log("Hey! Eyes on me!")}} type="Button">Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const consolelog = ()=>{console.log("Entering password...")}
     return (
-        <div></div>
+        
+        <div>
+           <input onChange={consolelog} type="password" />
+        </div>
     )
 }
 
